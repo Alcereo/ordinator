c5cbae451af2dd30cfaaf36bd8481b7a741c75cf
Aleksandr Kucheev <kucheev@yandex-team.ru>
2019-08-14 16:15:28 +0300

    Refactor stub server to sort headers

--- stubServer/server.go
+++ stubServer/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
 )
 
 func main() {
@@ -18,7 +19,13 @@ func main() {
 func mainHandler(writer http.ResponseWriter, request *http.Request) {
 	_, _ = fmt.Fprintf(writer, "%s %s\n", request.Method, request.URL.String())
 	_, _ = fmt.Fprintf(writer, "\n")
-	for header, value := range request.Header {
-		_, _ = fmt.Fprintf(writer, "%s=%v\n", header, value)
+
+	var headerKeys []string
+	for header := range request.Header {
+		headerKeys = append(headerKeys, header)
+	}
+	sort.Strings(headerKeys)
+	for _, headerKey := range headerKeys {
+		_, _ = fmt.Fprintf(writer, "%s=%v\n", headerKey, request.Header.Get(headerKey))
 	}
 }
