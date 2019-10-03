0ec88a1a76f446e0ee1eda19fca459d56a2accbf
Aleksandr Kucheev <kucheev@yandex-team.ru>
2019-10-03 13:26:58 +0300

    Stub creating extended

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,16 +1,12 @@
 package integration_test
 
 import (
-	"encoding/json"
-	"fmt"
+	. "github.com/Alcereo/ordinator/integration/utils"
 	. "github.com/Alcereo/ordinator/pkg/context"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"regexp"
 	"testing"
 )
 
@@ -23,139 +19,14 @@ var server *http.Server
 var googleApiStub *httptest.Server
 var resourceStub *httptest.Server
 
-type RequestMock struct {
-	request  Request
-	response Response
-}
-
-type Header struct {
-	name   string
-	regexp string
-}
-
-type URLPropsBody struct {
-	props map[string]string
-}
-
-func (check URLPropsBody) checkBody(body []byte, req *http.Request) error {
-
-	values, err := url.ParseQuery(string(body))
-	if err != nil {
-		return fmt.Errorf("parsing queries from body errror. %v", err.Error())
-	}
-
-	for key, value := range check.props {
-		if values.Get(key) != value {
-			return fmt.Errorf("property %v=%v not match with expected: %v", key, values.Get(key), value)
-		}
-	}
-
-	return nil
-}
-
-type Request struct {
-	method  string
-	url     string
-	headers []Header
-	body    []BodyCheck
-}
-
-type BodyCheck interface {
-	checkBody([]byte, *http.Request) error
-}
-
-type StringedBody interface {
-	getString() ([]byte, error)
-}
-
-type Response struct {
-	status  int
-	headers map[string]string
-	body    StringedBody
-}
-
-type jsonMap map[string]interface{}
-
-func (s jsonMap) getString() ([]byte, error) {
-	return json.Marshal(s)
-}
-
 var _ = BeforeSuite(func() {
 
-	googleApiStub = createServerStub([]RequestMock{
-		{
-			request: Request{
-				method: "POST",
-				url:    "/oauth2/v4/token",
-				headers: []Header{
-					{
-						name:   "Content-Type",
-						regexp: "application/x-www-form-urlencoded",
-					},
-				},
-				body: []BodyCheck{
-					URLPropsBody{
-						props: map[string]string{
-							"code":          "google-auth-code",
-							"client_id":     "google-client-id-1",
-							"client_secret": "google-client-secret",
-							"redirect_uri":  "http://localhost:8080/authentication/google",
-							"grant_type":    "authorization_code",
-						},
-					},
-				},
-			},
-			response: Response{
-				status: 200,
-				headers: map[string]string{
-					"Content-Type": "application/json;charset=UTF-8",
-				},
-				body: jsonMap{
-					"access_token":  "access-token-1",
-					"refresh_token": "refresh-token-1",
-					"expires_in":    9999,
-					"token_type":    "authorization_code",
-				},
-			},
-		},
-	})
-
-	resourceStub = createServerStub([]RequestMock{
-		{
-			request: Request{
-				method: "GET",
-				url:    "/api/v2/resource",
-			},
-			response: Response{
-				status:  200,
-				headers: nil,
-				body: jsonMap{
-					"status":  "OK",
-					"service": "resource",
-					"version": "v2",
-				},
-			},
-		},
-		{
-			request: Request{
-				method: "GET",
-				url:    "/api/v1/resource",
-			},
-			response: Response{
-				status:  200,
-				headers: nil,
-				body: jsonMap{
-					"status":  "OK",
-					"service": "resource",
-					"version": "v1",
-				},
-			},
-		},
-	})
-
+	googleApiStub = createGoogleApiStub()
+	resourceStub = createResourceServiceStub()
 	context := NewContext()
 
 	cacheAdapterIdentifier := "main-adapter"
+
 	context.SetupCache([]CacheAdapter{
 		{
 			Identifier:             cacheAdapterIdentifier,
@@ -237,68 +108,79 @@ var _ = BeforeSuite(func() {
 	}()
 })
 
-func createServerStub(mocks []RequestMock) *httptest.Server {
-	mux := http.NewServeMux()
+func createResourceServiceStub() *httptest.Server {
+	return CreateServiceStub([]RequestMock{
+		{
+			Request: Request{
+				Method: "GET",
+				Url:    "/api/v2/resource",
+			},
+			Response: Response{
+				Status:  200,
+				Headers: nil,
+				Body: JsonMap{
+					"status":  "OK",
+					"service": "resource",
+					"version": "v2",
+				},
+			},
+		},
+		{
+			Request: Request{
+				Method: "GET",
+				Url:    "/api/v1/resource",
+			},
+			Response: Response{
+				Status:  200,
+				Headers: nil,
+				Body: JsonMap{
+					"status":  "OK",
+					"service": "resource",
+					"version": "v1",
+				},
+			},
+		},
+	})
+}
 
-	for _, mReg := range mocks {
-		mux.HandleFunc(mReg.request.url, func(writer http.ResponseWriter, request *http.Request) {
-			if request.Method != mReg.request.method {
-				writer.WriteHeader(404)
-				_, _ = fmt.Fprint(writer, "Request Method 'POST' expected. Actual: '"+request.Method+"'.")
-				return
-			}
-
-			for _, check := range mReg.request.headers {
-				header := request.Header.Get(check.name)
-				matched, err := regexp.MatchString(check.regexp, header)
-				if err != nil {
-					writer.WriteHeader(500)
-					_, _ = fmt.Fprint(writer, "Parsing header regexp error: "+check.regexp+". Detail: "+err.Error())
-					return
-				}
-				if !matched {
-					writer.WriteHeader(400)
-					_, _ = fmt.Fprint(writer, "Header not matched regexp. Header: "+header+". Regexp: "+check.regexp)
-					return
-				}
-			}
-
-			bytes, err := ioutil.ReadAll(request.Body)
-			if err != nil {
-				writer.WriteHeader(500)
-				_, _ = fmt.Fprint(writer, "Reading body error: "+err.Error())
-				return
-			}
-
-			for _, check := range mReg.request.body {
-				err := check.checkBody(bytes, request)
-				if err != nil {
-					writer.WriteHeader(400)
-					_, _ = fmt.Fprint(writer, "Body not match: "+err.Error())
-					return
-				}
-			}
-
-			writer.WriteHeader(mReg.response.status)
-			for header, value := range mReg.response.headers {
-				writer.Header().Add(header, value)
-			}
-			bodyBytes, err := mReg.response.body.getString()
-			if err != nil {
-				writer.WriteHeader(500)
-				_, _ = fmt.Fprint(writer, "Writing body error: "+err.Error())
-				return
-			}
-			_, err = fmt.Fprint(writer, string(bodyBytes))
-			if err != nil {
-				writer.WriteHeader(500)
-				_, _ = fmt.Fprint(writer, "Writing body error: "+err.Error())
-				return
-			}
-		})
-	}
-
-	return httptest.NewServer(mux)
+func createGoogleApiStub() *httptest.Server {
+	return CreateServiceStub([]RequestMock{
+		{
+			Request: Request{
+				Method: "POST",
+				Url:    "/oauth2/v4/token",
+				Headers: []Header{
+					{
+						Name:   "Content-Type",
+						Regexp: "application/x-www-form-urlencoded",
+					},
+				},
+				Body: []BodyCheck{
+					URLPropsBody{
+						Props: map[string]string{
+							"code":          "google-auth-code",
+							"client_id":     "google-client-id-1",
+							"client_secret": "google-client-secret",
+							"redirect_uri":  "http://localhost:8080/authentication/google",
+							"grant_type":    "authorization_code",
+						},
+					},
+				},
+			},
+			Response: Response{
+				Status: 200,
+				Headers: map[string]string{
+					"Content-Type": "application/json;charset=UTF-8",
+				},
+				Body: JsonMap{
+					"access_token":  "access-token-1",
+					"refresh_token": "refresh-token-1",
+					"expires_in":    9999,
+					"token_type":    "authorization_code",
+				},
+			},
+		},
+	})
 }
 
 var _ = AfterSuite(func() {
---
+++ integration/utils/serviceStub.go
@@ -0,0 +1,132 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"regexp"
+)
+
+func CreateServiceStub(mocks []RequestMock) *httptest.Server {
+	mux := http.NewServeMux()
+
+	for _, mReg := range mocks {
+		mux.HandleFunc(mReg.Request.Url, func(writer http.ResponseWriter, request *http.Request) {
+			if request.Method != mReg.Request.Method {
+				writer.WriteHeader(404)
+				_, _ = fmt.Fprint(writer, "Request Method 'POST' expected. Actual: '"+request.Method+"'.")
+				return
+			}
+
+			for _, check := range mReg.Request.Headers {
+				header := request.Header.Get(check.Name)
+				matched, err := regexp.MatchString(check.Regexp, header)
+				if err != nil {
+					writer.WriteHeader(500)
+					_, _ = fmt.Fprint(writer, "Parsing header regexp error: "+check.Regexp+". Detail: "+err.Error())
+					return
+				}
+				if !matched {
+					writer.WriteHeader(400)
+					_, _ = fmt.Fprint(writer, "Header not matched regexp. Header: "+header+". Regexp: "+check.Regexp)
+					return
+				}
+			}
+
+			bytes, err := ioutil.ReadAll(request.Body)
+			if err != nil {
+				writer.WriteHeader(500)
+				_, _ = fmt.Fprint(writer, "Reading body error: "+err.Error())
+				return
+			}
+
+			for _, check := range mReg.Request.Body {
+				err := check.checkBody(bytes, request)
+				if err != nil {
+					writer.WriteHeader(400)
+					_, _ = fmt.Fprint(writer, "Body not match: "+err.Error())
+					return
+				}
+			}
+
+			writer.WriteHeader(mReg.Response.Status)
+			for header, value := range mReg.Response.Headers {
+				writer.Header().Add(header, value)
+			}
+			bodyBytes, err := mReg.Response.Body.getString()
+			if err != nil {
+				writer.WriteHeader(500)
+				_, _ = fmt.Fprint(writer, "Writing body error: "+err.Error())
+				return
+			}
+			_, err = fmt.Fprint(writer, string(bodyBytes))
+			if err != nil {
+				writer.WriteHeader(500)
+				_, _ = fmt.Fprint(writer, "Writing body error: "+err.Error())
+				return
+			}
+		})
+	}
+
+	return httptest.NewServer(mux)
+}
+
+type RequestMock struct {
+	Request  Request
+	Response Response
+}
+
+type Header struct {
+	Name   string
+	Regexp string
+}
+
+type URLPropsBody struct {
+	Props map[string]string
+}
+
+func (check URLPropsBody) checkBody(body []byte, req *http.Request) error {
+
+	values, err := url.ParseQuery(string(body))
+	if err != nil {
+		return fmt.Errorf("parsing queries from body errror. %v", err.Error())
+	}
+
+	for key, value := range check.Props {
+		if values.Get(key) != value {
+			return fmt.Errorf("property %v=%v not match with expected: %v", key, values.Get(key), value)
+		}
+	}
+
+	return nil
+}
+
+type Request struct {
+	Method  string
+	Url     string
+	Headers []Header
+	Body    []BodyCheck
+}
+
+type BodyCheck interface {
+	checkBody([]byte, *http.Request) error
+}
+
+type StringedBody interface {
+	getString() ([]byte, error)
+}
+
+type Response struct {
+	Status  int
+	Headers map[string]string
+	Body    StringedBody
+}
+
+type JsonMap map[string]interface{}
+
+func (s JsonMap) getString() ([]byte, error) {
+	return json.Marshal(s)
+}
